d733a12581190cb0310094cbd433a5a039e58af5
plankmannetje03 <38730@ma-web.nl>
2025-10-03 09:38:46 +0200

    klaar met les 3

--- index.php
+++ index.php
@@ -22,5 +22,28 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
+<?php
+
+
+for ($i = 0; $i < 10; $i++) {
+    echo "Waarde: $i<br>";
+}
+
+for ($i = 0; $i < 10; $i++) {
+    echo "Loop nummer: $i<br>";
+    $kwadraat = $i * $i;
+    echo "Kwadraat: $kwadraat<br>";
+}
+
+$array = ["a" => 1, "b" => 2, "c" => 3];
+foreach ($array as $key => $item) {
+    echo "Sleutel: $key, Waarde: $item<br>";
+}
+
+foreach ($array as $item) {
+    echo "Item: $item<br>";
+}
+?>
+
 </body>
 </html>
